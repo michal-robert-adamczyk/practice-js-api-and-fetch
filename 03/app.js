dfafe6f9ba44da15c1cf618e4c298c4abefdd1f2
Michal Adamczyk <michal.robert.adamczyk@gmail.com>
2020-04-23 13:20:59 +0200

    Exercise 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,34 +1,31 @@
 document.addEventListener('DOMContentLoaded', init);
 
+const button = document.querySelector('.button');
+const span = document.querySelector('.span');
+
 function init() {
 
     const promise = fetch('https://api.ipify.org');
 
-promise
-.then(resp => {
-    if(resp.ok) {
-        return resp.text()
-    }
+    promise
+        .then(resp => {
+            if (resp.ok) {
+                return resp.text()
+            }
 
-    return Promise.reject(resp);
-    })
-    .then(ip => console.log(ip))
-    .catch(err => console.error(err))
-    .finally(()=>{
-        console.log('Odpytywanie API zakończone!')
-    });
+            return Promise.reject(resp);
+        })
+        .then(ip => {
+            const IPValue = ip
+            function AddIPIntoSpan() {
+                span.innerHTML = IPValue;
+            }
+            button.addEventListener('click', AddIPIntoSpan);
 
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+            console.log('Odpytywanie API zakończone!')
+        });
 
-    console.log('DOM');
-    const button = document.querySelector('.button');
-    const span = document.querySelector('.span');
-    console.log(button);
-    console.log(span);
 }
-
-
-//po kliknięciu`<button/>` 
-//pobrać number *IP* z jakim łączysz się z Internetem 
-//i wstawić go do elementu `<span/>`.
-
-//Do rozwiązanie tego zadania możesz użyć *API* dostępnego pod tym adresem: [https://www.ipify.org](https://www.ipify.org/). Dane odbierane z *API* powinny być w formacie *JSON*.
