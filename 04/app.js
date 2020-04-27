1c3dfbc2494b35bf564dce82b26526cbbc1879a7
Michal Adamczyk <michal.robert.adamczyk@gmail.com>
2020-04-27 15:35:36 +0200

    read value from form fields and console log link to api

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,64 @@
-document.addEventListener('DOMContentLoaded', init);
+
+
 
 function init() {
-    console.log('DOM');
+
+
+    const submitButton = document.querySelector('.form__submit')
+
+
+
+
+    submitButton.addEventListener('click', consoleLogValue);
+
+    function consoleLogValue(e) {
+
+        const key = '5c2272e32ff1442098abf9ecd58d4df6';
+        const latitudeField = document.querySelector('.form__field--lat');
+        const longitudeField = document.querySelector('.form__field--lng');
+        const latitude = latitudeField.value;
+        const longitude = longitudeField.value;
+
+
+        e.preventDefault();
+        console.log(latitude);
+        console.log(longitude);
+        console.log(key);
+        const URL = `https://api.weatherbit.io/v2.0/current?key=[${key}]&lat=[${latitude}]&lon=[${longitude}]`;
+        console.log(URL);
+    }
+
+
 }
+
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+
+    // const promise = fetch('`https://api.weatherbit.io/v2.0/current?key=[key]&lat=[latitude]&lon=[longitude]`');
+
+    // promise
+    //     .then(resp => {
+    //         if (resp.ok) {
+    //             return resp.text()
+    //         }
+
+    //         return Promise.reject(resp);
+    //     })
+    //     .then(ip => {
+    //         const IPValue = ip
+    //         function AddIPIntoSpan() {
+    //             span.innerHTML = IPValue;
+    //         }
+    //         button.addEventListener('click', AddIPIntoSpan);
+
+    //     })
+    //     .catch(err => console.error(err))
+    //     .finally(() => {
+    //         console.log('Odpytywanie API zakończone!')
+    //     });
+
+
+
