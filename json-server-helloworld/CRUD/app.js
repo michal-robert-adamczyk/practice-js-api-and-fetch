cf10ecd43f7c222744c5c292e1fa7403532b9348
Michal Adamczyk <michal.robert.adamczyk@gmail.com>
2020-04-29 12:59:46 +0200

    Create and call function who remove trips

--- json-server-helloworld/CRUD/app.js
+++ json-server-helloworld/CRUD/app.js
@@ -6,51 +6,54 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     console.log('DOM loaded');
     loadExcursions();
+    removeExcursions();
 
-    function loadExcursions() {
-        fetch('http://localhost:3000/excursions')
-            .then(resp => {
-                if (resp.ok) { resp.json(); }
-                return Promise.reject(resp);
-            })
-            .then(data => {
-                console.log(data);
-                // insertExcursions(data);
-            })
-            // .then(data => {
-            //     removeExcursions(data);
-            // })
+   
+}
 
-            .catch(err => console.error(err));
-    }
+function loadExcursions() {
+    fetch(apiUrl)
+        .then(resp => {
+            if (resp.ok) {return resp.json(); }
+            return Promise.reject(resp);
+        })
+        .then(data => {
+            console.log(data);
+            insertExcursions(data);
+        })
+        // .then(data => {
+        //     removeExcursions(data);
+        // })
 
+        .catch(err => console.error(err));
 }
 
 
 
-// function insertExcursions(excursionsArr) {
-//     const ulEl = document.querySelector('.excursions');
-//     ulEl.innerHTML='';
-//     excursionsArr.forEach(item => {
-//         const liEl = document.createElement('li');
-//         liEl.dataset.id = item.id;
-//         liEl.classList.add('excursions__item');
-//         liEl.innerText = `${item.name}: ${item.price}PLN`;
+function insertExcursions(excursionsArr) {
+    const ulEl = document.querySelector('.excursions');
+    ulEl.innerHTML='';
+    excursionsArr.forEach(item => {
+        const liEl = document.createElement('li');
+        liEl.dataset.id = item.id;
+        liEl.classList.add('excursions__item');
+        liEl.innerText = `${item.name}: ${item.price}PLN`;
 
-//         ulEl.appendChild(liEl);
-//     });
-// }
-// function removeExcursions() {
-//     const ulEl = document.querySelector('.excursions');
-//     ulEl.addEventListener('click', e => {
-//         const targetEl = e.target;
-//         if(targetEl.tagName === 'LI'){
-//             const id = targetEl.dataset.id;
-//             const options = {method: 'DELETE'};
-//             fetch(`${apiUrl}/${id}`, options)
-//             .then(resp => console.log(resp))
-//             .catch(err => console.error(err))
-//             .finally(loadExcursions);
-//         }
-//     })
-// }
+        ulEl.appendChild(liEl);
+    });
+}
+
+function removeExcursions() {
+    const ulEl = document.querySelector('.excursions');
+    ulEl.addEventListener('click', e => {
+        const targetEl = e.target;
+        if(targetEl.tagName === 'LI'){
+            const id = targetEl.dataset.id;
+            const options = {method: 'DELETE'};
+            fetch(`${apiUrl}/${id}`, options)
+            .then(resp => console.log(resp))
+            .catch(err => console.error(err))
+            .finally(loadExcursions);
+        }
+    })
+}
