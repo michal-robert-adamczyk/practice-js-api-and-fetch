ef3adeb72243f090de8fb354670436f14325a4b0
Michal Adamczyk <michal.robert.adamczyk@gmail.com>
2020-04-30 12:13:04 +0200

    Repair JSON server and create readme.md

--- json-server-helloworld/CRUD/app.js
+++ json-server-helloworld/CRUD/app.js
@@ -7,23 +7,21 @@ function init() {
     console.log('DOM loaded');
     loadExcursions();
     removeExcursions();
+    addExcursion();
+    updateExcursions();
 
-   
 }
 
 function loadExcursions() {
     fetch(apiUrl)
         .then(resp => {
-            if (resp.ok) {return resp.json(); }
+            if (resp.ok) { return resp.json(); }
             return Promise.reject(resp);
         })
         .then(data => {
             console.log(data);
             insertExcursions(data);
         })
-        // .then(data => {
-        //     removeExcursions(data);
-        // })
 
         .catch(err => console.error(err));
 }
@@ -32,12 +30,16 @@ function loadExcursions() {
 
 function insertExcursions(excursionsArr) {
     const ulEl = document.querySelector('.excursions');
-    ulEl.innerHTML='';
+    ulEl.innerHTML = '';
     excursionsArr.forEach(item => {
         const liEl = document.createElement('li');
         liEl.dataset.id = item.id;
         liEl.classList.add('excursions__item');
-        liEl.innerText = `${item.name}: ${item.price}PLN`;
+        liEl.innerHTML = `
+        [<a href="#">usuń</a>]
+        <span>${item.name}</span>:
+        <span>${item.price}</span> PLN
+        <button>edytuj</button>`;
 
         ulEl.appendChild(liEl);
     });
@@ -47,13 +49,80 @@ function removeExcursions() {
     const ulEl = document.querySelector('.excursions');
     ulEl.addEventListener('click', e => {
         const targetEl = e.target;
-        if(targetEl.tagName === 'LI'){
-            const id = targetEl.dataset.id;
-            const options = {method: 'DELETE'};
+        if (targetEl.tagName === 'A') {
+            const parentEl = targetEl.parentElement;
+            const id = parentEl.dataset.id;
+            const options = { method: 'DELETE' };
             fetch(`${apiUrl}/${id}`, options)
-            .then(resp => console.log(resp))
-            .catch(err => console.error(err))
-            .finally(loadExcursions);
+                .then(resp => console.log(resp))
+                .catch(err => console.error(err))
+                .finally(loadExcursions);
         }
     })
+}
+
+function addExcursion() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const { name, price } = e.target.elements;
+        const data = {
+            name: name.value, price: price.value
+        };
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        };
+        fetch(apiUrl, options)
+            .then(resp => {
+                console.log(resp);
+                console.log('addExcursion');
+            })
+            .catch(err => console.error(err))
+            .finally(loadExcursions);
+    });
+}
+
+function updateExcursions() {
+    const ulEl = document.querySelector('.excursions');
+    ulEl.addEventListener('click', e => {
+        const targetEl = e.target;
+        console.log(targetEl)
+        if (targetEl.tagName === 'BUTTON') {
+            const parentEl = targetEl.parentElement;
+            const spanList = parentEl.querySelectorAll('span');
+            const isEditable = [...spanList].every(span => span.isContentEditable);
+            if (isEditable) {
+                // zapisz dane w API
+                const id = parentEl.dataset.id;
+                const data = {
+                    name: spanList[0].innerText,
+                    price: spanList[1].innerText,
+                }
+                const options = { 
+                    method: 'PUT',
+                    body: JSON.stringify( data ),
+                    headers: { 'Content-Type': 'application/json' } 
+                };
+                fetch(`${apiUrl}/${id}`, options)
+                    .then(resp => console.log(resp))
+                    .catch(err => console.error(err))
+                    .finally( () => {
+                        targetEl.innerText = 'edytuj';
+                        spanList.forEach(
+                            span => span.contentEditable = false
+                        );
+                    });
+
+            } else {
+                //ustaw [contentEditable] na [true]
+                targetEl.innerText = 'zapisz';
+                spanList.forEach(
+                    span => span.contentEditable = true
+                );
+            }
+        }
+    });
 }
---
+++ json-server-helloworld/CRUD/readme.md
@@ -0,0 +1,23 @@
+## Utwórz zdarzenie, które uruchomi funkcję init() po załadowaniu drzewa DOM
+
+## Stwórz funkcję loadExcursions(), która pobierze dane z API
+
+<p> funkcja ma odwoływać się do odpowiedniego zasobu przez link w fetch() i wyświetlać dane w konsoli.
+Używam resp.json() do konwersji z JSON do odpowiedniej struktóry, w tym wypadku tablicy przechowującej obiekty.
+ </p>
+
+## Stwórz zmienną globalną dla adresu zasobu
+
+## Uruchom funkcję ładującą dane z API wewnątrz funkcji init()
+
+## Stwórz funkcję, która pobrane dane opakuje w znaczniki HTML
+
+<P>
+dane są w formie tablicy, dlatego używam forEach() gdzie zmienna item przechowuje obiekt dotyczący jednej wycieczki
+</p>
+
+
+## Stwórz funkcję, ktora będzie usuwała elementów po kliknięciu w jeden z nich
+
+<p> Wykorzystuję tutaj propagację zdarzeń, dzieki temu usuwam dane z API o ID zapisanym w dataset elementu li <p>
+
