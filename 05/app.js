72a312767a36d372b432b627da78afa9f1e68952
Michal Adamczyk <michal.robert.adamczyk@gmail.com>
2020-04-30 13:35:39 +0200

    Make API to handle form fields and adding users

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,17 @@
+//napisanie obsługi formularza, która pozwoli na dodawanie danych do naszego lokalnego API.
+// * wykorzystać odpowiednią metodę (`POST`)
+// * utworzyć obiekt na podstawie wysłanych przez formularz dancyh, który trzeba zamienić na format *JSON* (`JSON.stringify()`)
+// * przekazanć odpowiedni nagłówek (`'Content-Type': 'application/json'`)
+
+// Po dodaniu kolejnego użytkownika należy zaktualizować widok przy pomocy funkcji `loadUsers()`, którą należy uruchomić w odpowiednim momencie np. `finally()`.
+
 const apiUrl = 'http://localhost:3000/users';
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    addUser();
 }
 
 function loadUsers() {
@@ -33,5 +41,33 @@ function insertUsers(usersList) {
         liElement.innerText = `${user.firstName} ${user.lastName}`;
 
         ulElement.appendChild(liElement);
+    });
+}
+
+function addUser() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const firstNameToSetAtribute = document.querySelector('.form__field--first-name');
+        const lastNameToSetAtribute = document.querySelector('.form__field--last-name')
+                firstNameToSetAtribute.setAttribute('name', 'firstName');
+        lastNameToSetAtribute.setAttribute('name', 'lastName');
+        const { firstName, lastName } = e.target.elements;
+        const data = {
+            firstName: firstName.value, lastName: lastName.value
+        };
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        };
+        fetch(apiUrl, options)
+            .then(resp => {
+                console.log(resp);
+                console.log('addUser');
+            })
+            .catch(err => console.error(err))
+            .finally(loadUsers);
     });
 }
